22321f9b2e1cad2432b38397a7217dc7075e8014
Kelsey Maass <kmaass@uw.edu>
2020-03-11 16:03:12 -0700

    changed tolerance to default

--- testExamples.m
+++ testExamples.m
@@ -1,89 +1,90 @@
-function tests = testOptimization
+function tests = testExamples
     addpath(genpath('PROSTATE'));
     addpath(genpath('minConf'));
     tests = functiontests(localfunctions);
 end
 
-function testExample1(testCase)
+function testExample1(~)
     unif = struct('name','PTV_68');
     unif.terms = {struct('type','unif','dose',81,'weight',1)};
     udvc = struct('name','Rectum');
     udvc.terms = {struct('type','udvc','dose',50,'percent',50,'weight',1)};
-    prob = FluenceMapOpt({unif,udvc},'tol',5e-5);
+    prob = FluenceMapOpt({unif,udvc});
     percentOver = sum(prob.structs{2}.A*prob.x0 > 50)/prob.structs{2}.nVoxels;
     assert(abs(percentOver - 0.5680) < 1e-4,'Incorrect initialization')
-    prob.calcBeamlets(false);
-    assert(prob.nIter == 231,'Wrong number of iterations')
+    prob.calcBeams(false);
+    assert(prob.nIter == 11,'Wrong number of iterations')
     percentOver = sum(prob.structs{2}.A*prob.x > 50)/prob.structs{2}.nVoxels;
-    assert(abs(percentOver - 0.5152) < 1e-4,'Incorrect solution')
+    assert(abs(percentOver - 0.5273) < 1e-4,'Incorrect solution')
 end
 
-function testExample2(testCase)
+function testExample2(~)
     unif = struct('name','PTV_68');
     unif.terms = {struct('type','unif','dose',81,'weight',1)};
     udvc = struct('name','Rectum');
     udvc.terms = {struct('type','udvc','dose',30,'percent',30,'weight',1)};
-    prob = FluenceMapOpt({unif,udvc},'tol',5e-5);
+    prob = FluenceMapOpt({unif,udvc});
     percentOver = sum(prob.structs{2}.A*prob.x0 > 30)/prob.structs{2}.nVoxels;
     assert(abs(percentOver - 0.6414) < 1e-4,'Incorrect initialization')
-    prob.calcBeamlets(false);
-    assert(prob.nIter == 468,'Wrong number of iterations')
+    prob.calcBeams(false);
+    assert(prob.nIter == 42,'Wrong number of iterations')
     percentOver = sum(prob.structs{2}.A*prob.x > 30)/prob.structs{2}.nVoxels;
-    assert(abs(percentOver - 0.3337) < 1e-4,'Incorrect solution')
+    assert(abs(percentOver - 0.3416) < 1e-4,'Incorrect solution')
 end
 
-function testExample3(testCase)
+function testExample3(~)
     unif = struct('name','PTV_68');
     unif.terms = {struct('type','unif','dose',81,'weight',1)};
     udvc = struct('name','Rectum');
     udvc.terms = {struct('type','udvc','dose',10,'percent',10,'weight',1)};
-    prob = FluenceMapOpt({unif,udvc},'tol',5e-5);
+    prob = FluenceMapOpt({unif,udvc});
     percentOver = sum(prob.structs{2}.A*prob.x0 > 10)/prob.structs{2}.nVoxels;
     assert(abs(percentOver - 0.7397) < 1e-4,'Incorrect initialization')
-    prob.calcBeamlets(false);
-    assert(prob.nIter == 320,'Wrong number of iterations')
+    prob.calcBeams(false);
+    assert(prob.nIter == 30,'Wrong number of iterations')
     percentOver = sum(prob.structs{2}.A*prob.x > 10)/prob.structs{2}.nVoxels;
-    assert(abs(percentOver - 0.2391) < 1e-4,'Incorrect solution')
+    assert(abs(percentOver - 0.2476) < 1e-4,'Incorrect solution')
 end
 
-function testExample4(testCase)
+function testExample4(~)
     unif = struct('name','PTV_68');
-    unif.terms = {struct('type','unif','dose',81,'weight',1)};
+    unif.terms = {struct('type','unif','dose',81,'weight',1),...
+        struct('type','ldvc','dose',81','percent',5,'weight',1),...
+        struct('type','udvc','dose',85,'percent',0,'weight',1)};
     udvc = struct('name','Rectum');
     udvc.terms = {struct('type','udvc','dose',20,'percent',60,'weight',1),...
         struct('type','udvc','dose',50,'percent',50,'weight',1),...
-        struct('type','udvc','dose',60,'percent',20,'weight',1),...
-        struct('type','udvc','dose',75,'percent',0,'weight',1)};
+        struct('type','udvc','dose',60,'percent',20,'weight',1)};
 
     % Check initialization
-    prob = FluenceMapOpt({unif,udvc},'tol',5e-5,'maxIter',1000);
+    prob = FluenceMapOpt({unif,udvc});
+    percentUnder = sum(prob.structs{1}.A*prob.x0 < 81)/prob.structs{1}.nVoxels;
+    assert(abs(percentUnder - 0.5326) < 1e-4,'Incorrect initialization')
+    percentOver = sum(prob.structs{1}.A*prob.x0 > 85)/prob.structs{1}.nVoxels;
+    assert(abs(percentOver - 0) < 1e-4,'Incorrect initialization')
     percentOver = sum(prob.structs{2}.A*prob.x0 > 20)/prob.structs{2}.nVoxels;
     assert(abs(percentOver - 0.6881) < 1e-4,'Incorrect initialization')
     percentOver = sum(prob.structs{2}.A*prob.x0 > 50)/prob.structs{2}.nVoxels;
     assert(abs(percentOver - 0.5680) < 1e-4,'Incorrect initialization')
     percentOver = sum(prob.structs{2}.A*prob.x0 > 60)/prob.structs{2}.nVoxels;
     assert(abs(percentOver - 0.3847) < 1e-4,'Incorrect initialization') 
-    percentOver = sum(prob.structs{2}.A*prob.x0 > 75)/prob.structs{2}.nVoxels;
-    assert(abs(percentOver - 0.1341) < 1e-4,'Incorrect initialization')
    
-    % Check solution (doesn't match paper)
-    prob.calcBeamlets(false);
-    %assert(prob.nIter == 568,'Wrong number of iterations')
-    assert(prob.nIter == 513,'Wrong number of iterations')
+    % Check solution
+    prob.calcBeams(false);
+    assert(prob.nIter == 288,'Wrong number of iterations')
+    percentUnder = sum(prob.structs{1}.A*prob.x < 81)/prob.structs{1}.nVoxels;
+    assert(abs(percentUnder - 0.5589) < 1e-4,'Incorrect initialization')
+    percentOver = sum(prob.structs{1}.A*prob.x > 85)/prob.structs{1}.nVoxels;
+    assert(abs(percentOver - 0) < 1e-4,'Incorrect initialization')
     percentOver = sum(prob.structs{2}.A*prob.x > 20)/prob.structs{2}.nVoxels;
-    assert(abs(percentOver - 0.6141) < 1e-4,'Incorrect solution')
+    assert(abs(percentOver - 0.6135) < 1e-4,'Incorrect solution')
     percentOver = sum(prob.structs{2}.A*prob.x > 50)/prob.structs{2}.nVoxels;
-    %assert(abs(percentOver - 0.4660) < 1e-4,'Incorrect solution')
-    assert(abs(percentOver - 0.4678) < 1e-4,'Incorrect solution')
+    assert(abs(percentOver - 0.4527) < 1e-4,'Incorrect solution')
     percentOver = sum(prob.structs{2}.A*prob.x > 60)/prob.structs{2}.nVoxels;
-    %assert(abs(percentOver - 0.2172) < 1e-4,'Incorrect solution')
-    assert(abs(percentOver - 0.2166) < 1e-4,'Incorrect solution')
-    percentOver = sum(prob.structs{2}.A*prob.x > 75)/prob.structs{2}.nVoxels;
-    %assert(abs(percentOver - 0.0388) < 1e-4,'Incorrect solution')
-    assert(abs(percentOver - 0.0394) < 1e-4,'Incorrect solution')
+    assert(abs(percentOver - 0.2324) < 1e-4,'Incorrect solution')
 end
 
-function testExample5(testCase)
+function testExample5(~)
     unif1 = struct('name','PTV_68');
     unif1.terms = {struct('type','unif','dose',81,'weight',1)};
     unif2 = struct('name','PTV_56');
@@ -94,17 +95,17 @@ function testExample5(testCase)
     udvc2.terms = {struct('type','udvc','dose',30,'percent',30,'weight',1)};
     
     % Check initialization
-    prob = FluenceMapOpt({unif1,udvc1,udvc2,unif2},'tol',5e-5);
-    percentOver = sum(prob.structs{2}.A*prob.x0 > 50)/prob.structs{2}.nVoxels;
-    assert(abs(percentOver - 0.8204) < 1e-4,'Incorrect initialization')
-    percentOver = sum(prob.structs{3}.A*prob.x0 > 30)/prob.structs{3}.nVoxels;
-    assert(abs(percentOver - 0.9221) < 1e-4,'Incorrect initialization')
+    prob = FluenceMapOpt({unif1,unif2,udvc1,udvc2});
+    percentOver = sum(prob.structs{3}.A*prob.x0 > 50)/prob.structs{3}.nVoxels;
+    assert(abs(percentOver - 0.8247) < 1e-4,'Incorrect initialization')
+    percentOver = sum(prob.structs{4}.A*prob.x0 > 30)/prob.structs{4}.nVoxels;
+    assert(abs(percentOver - 0.9187) < 1e-4,'Incorrect initialization')
     
     % Check solution
-    prob.calcBeamlets(true);
-    assert(prob.nIter == 336,'Wrong number of iterations')
-    percentOver = sum(prob.structs{2}.A*prob.x > 50)/prob.structs{2}.nVoxels;
-    assert(abs(percentOver - 0.5704) < 1e-4,'Incorrect solution')
-    percentOver = sum(prob.structs{3}.A*prob.x > 30)/prob.structs{3}.nVoxels;
-    assert(abs(percentOver - 0.3576) < 1e-4,'Incorrect solution')
+    prob.calcBeams(false);
+    assert(prob.nIter == 75,'Wrong number of iterations')
+    percentOver = sum(prob.structs{3}.A*prob.x > 50)/prob.structs{3}.nVoxels;
+    assert(abs(percentOver - 0.5691) < 1e-4,'Incorrect solution')
+    percentOver = sum(prob.structs{4}.A*prob.x > 30)/prob.structs{4}.nVoxels;
+    assert(abs(percentOver - 0.3741) < 1e-4,'Incorrect solution')
 end
