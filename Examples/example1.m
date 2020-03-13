4e2650480d37b0e4dd13048abd8eec7751c375b7
Kelsey Maass <kmaass@uw.edu>
2020-03-13 16:26:49 -0700

    condensed example 1

---
+++ Examples/example1.m
@@ -0,0 +1,50 @@
+% One PTV and one OAR with one dose-volume constraint
+
+clear all; close all; clc;
+
+% Add data and functions to path
+currentFolder = pwd;
+cd ..
+addpath(genpath(pwd));
+cd(currentFolder);
+
+% Loop over different dose/percent values
+labels = ['a' 'b' 'c'];
+doses = [50 30 10];
+for ii = 1:length(doses)
+    fprintf('\nExample 1%s\n',labels(ii));
+    
+    % PTV - prostate
+    prostate.name = 'PTV_68';
+    prostate.terms = {struct('type','unif','dose',81,'weight',1)};
+
+    % OAR - rectum
+    rectum.name = 'Rectum';
+    rectum.terms = {struct('type','udvc','dose',doses(ii),...
+        'percent',doses(ii),'weight',1)};
+
+    % Create problem instance
+    structs = {prostate,rectum};
+    prob = FluenceMapOpt(structs);
+
+    % Calculate approximate dose
+    fprintf('\nCalculating approximate dose\n');
+    prob.calcBeams();
+    fprintf('\nIterations: %d, Time: %.2f\n',prob.nIter,prob.time);
+    filename1 = ['e1' labels(ii) '_x1.mat'];
+    prob.saveResults(filename1);
+
+    % Calculate polished dose
+    fprintf('\nCalculating polished dose\n');
+    prob.calcBeamsPolish(prob.x);
+    fprintf('\nTime: %.2f\n',prob.time);
+    filename2 = ['e1' labels(ii) '_x2.mat'];
+    prob.saveResults(filename2);
+
+    % Calculate approximate dose with continuation
+    fprintf('\nCalculating approximate dose with continuation\n');
+    prob = calcBeamsCont(prob,structs,true);
+    fprintf('\nIterations: %d, Time: %.2f\n',prob.nIter,prob.time);
+    filename3 = ['e1' labels(ii) '_x3.mat'];
+    prob.saveResults(filename3);
+end
---
+++ Examples/example2.m
@@ -0,0 +1,44 @@
+% One PTV and one OAR with multiple dose-volume constraints
+
+clear all; close all; clc;
+
+% Add data and functions to path
+currentFolder = pwd;
+cd ..
+addpath(genpath(pwd));
+cd(currentFolder);
+
+% PTV - prostate
+prostate.name = 'PTV_68';
+prostate.terms = {struct('type','unif','dose',81,'weight',1),...
+    struct('type','ldvc','dose',81,'percent',5,'weight',1),...
+    struct('type','udvc','dose',85,'percent',0,'weight',1)};
+
+% OAR - rectum
+rectum.name = 'Rectum';
+rectum.terms = {struct('type','udvc','dose',20,'percent',60,'weight',1),...
+    struct('type','udvc','dose',50,'percent',50,'weight',1),...
+    struct('type','udvc','dose',60,'percent',20,'weight',1)};
+
+% Create problem instance
+fprintf('Example 2\n');
+structs = {prostate,rectum};
+prob = FluenceMapOpt(structs);
+
+% Calculate approximate dose
+fprintf('\nCalculating approximate dose\n');
+prob.calcBeams();
+fprintf('\nIterations: %d, Time: %.2f\n\n',prob.nIter,prob.time);
+prob.saveResults('e2_x1.mat');
+
+% Calculate polished dose
+fprintf('\nCalculating polished dose\n');
+prob.calcBeamsPolish(prob.x);
+fprintf('\nTime: %.2f\n\n',prob.time);
+prob.saveResults('e2_x2.mat');
+
+% Calculate approximate dose with continuation
+fprintf('\nCalculating approximate dose with continuation\n');
+prob = calcBeamsCont(prob,structs,true);
+fprintf('\nIterations: %d, Time: %.2f\n\n',prob.nIter,prob.time);
+prob.saveResults('e2_x3.mat');
