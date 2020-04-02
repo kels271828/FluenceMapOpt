0a502d90dd96f50875ad05773acefaf8aff3c5e2
Kelsey Maass <kmaass@uw.edu>
2020-04-01 19:54:35 -0700

    removed old notebooks

--- Tests/testExamples.m
+++ Tests/testExamples.m
@@ -55,7 +55,7 @@ function testExample4(~)
         struct('type','udvc','dose',85,'percent',0,'weight',1)};
     udvc.name = 'Rectum';
     udvc.terms = {struct('type','udvc','dose',20,'percent',60,'weight',1),...
-        struct('type','udvc','dose',50,'percent',50,'weight',1),...
+        struct('type','udvc','dose',40,'percent',40,'weight',1),...
         struct('type','udvc','dose',60,'percent',20,'weight',1)};
 
     % Check initialization
@@ -66,24 +66,24 @@ function testExample4(~)
     assert(abs(percentOver - 0) < 1e-4,'Incorrect initialization')
     percentOver = sum(prob.structs{2}.A*prob.x0 > 20)/prob.structs{2}.nVoxels;
     assert(abs(percentOver - 0.6881) < 1e-4,'Incorrect initialization')
-    percentOver = sum(prob.structs{2}.A*prob.x0 > 50)/prob.structs{2}.nVoxels;
-    assert(abs(percentOver - 0.5680) < 1e-4,'Incorrect initialization')
+    percentOver = sum(prob.structs{2}.A*prob.x0 > 40)/prob.structs{2}.nVoxels;
+    assert(abs(percentOver - 0.6129) < 1e-4,'Incorrect initialization')
     percentOver = sum(prob.structs{2}.A*prob.x0 > 60)/prob.structs{2}.nVoxels;
     assert(abs(percentOver - 0.3847) < 1e-4,'Incorrect initialization') 
    
     % Check solution
     prob.calcBeams(false);
-    assert(prob.nIter == 288,'Wrong number of iterations')
+    assert(prob.nIter == 32,'Wrong number of iterations')
     percentUnder = sum(prob.structs{1}.A*prob.x < 81)/prob.structs{1}.nVoxels;
-    assert(abs(percentUnder - 0.5589) < 1e-4,'Incorrect initialization')
+    assert(abs(percentUnder - 0.5594) < 1e-4,'Incorrect initialization')
     percentOver = sum(prob.structs{1}.A*prob.x > 85)/prob.structs{1}.nVoxels;
     assert(abs(percentOver - 0) < 1e-4,'Incorrect initialization')
     percentOver = sum(prob.structs{2}.A*prob.x > 20)/prob.structs{2}.nVoxels;
-    assert(abs(percentOver - 0.6135) < 1e-4,'Incorrect solution')
-    percentOver = sum(prob.structs{2}.A*prob.x > 50)/prob.structs{2}.nVoxels;
-    assert(abs(percentOver - 0.4527) < 1e-4,'Incorrect solution')
+    assert(abs(percentOver - 0.6280) < 1e-4,'Incorrect solution')
+    percentOver = sum(prob.structs{2}.A*prob.x > 40)/prob.structs{2}.nVoxels;
+    assert(abs(percentOver - 0.4399) < 1e-4,'Incorrect solution')
     percentOver = sum(prob.structs{2}.A*prob.x > 60)/prob.structs{2}.nVoxels;
-    assert(abs(percentOver - 0.2324) < 1e-4,'Incorrect solution')
+    assert(abs(percentOver - 0.2330) < 1e-4,'Incorrect solution')
 end
 
 function testExample5(~)
@@ -105,9 +105,9 @@ function testExample5(~)
     
     % Check solution
     prob.calcBeams(false);
-    assert(prob.nIter == 75,'Wrong number of iterations')
+    assert(prob.nIter == 48,'Wrong number of iterations')
     percentOver = sum(prob.structs{3}.A*prob.x > 50)/prob.structs{3}.nVoxels;
-    assert(abs(percentOver - 0.5691) < 1e-4,'Incorrect solution')
+    assert(abs(percentOver - 0.5733) < 1e-4,'Incorrect solution')
     percentOver = sum(prob.structs{4}.A*prob.x > 30)/prob.structs{4}.nVoxels;
-    assert(abs(percentOver - 0.3741) < 1e-4,'Incorrect solution')
+    assert(abs(percentOver - 0.3814) < 1e-4,'Incorrect solution')
 end
