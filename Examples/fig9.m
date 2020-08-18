b8c68252020929ca2bcd0325f773e910362cef71
Kelsey Maass <kmaass@uw.edu>
2020-08-18 14:33:23 -0700

    updated figure 9

--- Examples/fig9.m
+++ Examples/fig9.m
@@ -18,7 +18,7 @@ rectum.name = 'Rectum';
 rectum.terms = {struct('type','udvc','dose',30,'percent',30,'weight',1)};
 
 % Create problem instance
-fprintf('\nExample 1\n');
+fprintf('Example 1\n\n');
 structs = {prostate,rectum};
 prob = FluenceMapOpt(structs);    
 xMat = prob.x0;
@@ -35,26 +35,26 @@ t = results.time;
 disp('Approximate dose')
 fprintf('OAR %% > 30 Gy: %.2f, PTV D95: %.2f, Time: %.2f\n\n',...
     prob.getPercent(2,1,30,x),prob.getPercentile(1,0.95,x),t);
-% OAR % > 30 Gy: 34.16, PTV D95: 79.17, Time: 6.38
+% OAR % > 30 Gy: 34.16, PTV D95: 79.17, Time: 7.04 (total 7.22)
 
-% Load approximate dose with continuation 1
-load('ex1Results/ex1Continue1.mat')
+% Load approximate dose with continuation (a)
+load('ex1Results/ex1ContinueA.mat')
 x = results.x;
 xMat = [xMat x];
 t = results.time;
-disp('Approximate dose with continuation 1')
+disp('Approximate dose with continuation (a)')
 fprintf('OAR %% > 30 Gy: %.2f, PTV D95: %.2f, Time: %.2f\n\n',...
     prob.getPercent(2,1,30,x),prob.getPercentile(1,0.95,x),t);
-% OAR % > 30 Gy: 29.61, PTV D95: 79.03, Time: 0.84 (total 32.81)
+% OAR % > 30 Gy: 29.61, PTV D95: 79.03, Time: 33.43 (total 33.61)
 
-% Load approximate dose with continuation 2
-load('ex1Results/ex1Continue2.mat')
+% Load approximate dose with continuation (b)
+load('ex1Results/ex1ContinueB.mat')
 x = results.x;
 xMat = [xMat x];
 t = results.time;
-disp('Approximate dose with continuation 2')
+disp('Approximate dose with continuation (b)')
 fprintf('OAR %% > 30 Gy: %.2f, PTV D95: %.2f, Time: %.2f\n\n',...
     prob.getPercent(2,1,30,x),prob.getPercentile(1,0.95,x),t);
-% OAR % > 30 Gy: 19.36, PTV D95: 78.02, Time: 3.37 (total 163.87)
+% OAR % > 30 Gy: 19.84, PTV D95: 78.05, Time: 163.33 (total 163.51)
 
 prob.plotDVHPaper(xMat)  
